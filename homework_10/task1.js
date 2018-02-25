20f714e933b9a048db68ccfc14029872d48d91da
Pavlo Taras <pavlo.taras1985@gmail.com>
2018-02-25 22:31:23 +0200

    Added example code for task1.js for Homework 10

--- homework_10/task1.js
+++ homework_10/task1.js
@@ -15,3 +15,29 @@ const debounce = (callback, ms) => {
         timeoutId = setTimeout(stop, ms)
     }
 }
+
+// Example
+
+let iterator = 0;
+
+function increaseIteratorBy1() {
+  iterator++;
+
+  printIteratorValue();
+}
+
+function printIteratorValue() {
+  console.log('Iterator value ', iterator);
+}
+
+var increaseIterator = debounce(increaseIteratorBy1, 1000);
+
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator();
+increaseIterator(); // Should print 'Iterator value 2'
