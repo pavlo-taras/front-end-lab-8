52c8a31dc4a71445ca01f09ead301a96767b5983
Pavlo Taras <pavlo.taras1985@gmail.com>
2018-03-20 13:49:51 +0200

    Added code for hometask 2 offline

---
+++ homework_02/js/script.js
@@ -0,0 +1,26 @@
+﻿$(window).load(function(){
+    $('.flexslider').flexslider({
+      animation: "fade"
+    });
+});  
+
+$(window).load(function(){
+    $(document).on('submit','.form',function(){
+    	if( $('.form #name').val().length < 6) {
+    		alert('Name must contain at least 6 characters');
+    		return false;
+    	}
+    });
+});
+
+$(window).load(function(){
+    var maxHeight = 0;
+    for( var i=0; i < $('.category-unit').length; i++) {
+        if ($('.category-unit').eq(i).height() > maxHeight) {
+            maxHeight = $('.category-unit').eq(i).height()
+        }
+    }
+    for( var i=0; i < $('.category-unit').length; i++) {
+        $('.category-unit').eq(i).css('height', maxHeight);
+    }
+});
