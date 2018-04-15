d78ae0e7bbcbb19c45091ca66c16b765f5b69d99
Pavlo Taras <pavlo.taras1985@gmail.com>
2018-04-15 23:57:11 +0300

    Added webpack configs files

---
+++ homework_09/webpack.common.js
@@ -0,0 +1,33 @@
+const webpack = require("webpack");
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: ['./src/app.js', './src/clock/canvasState.js', './src/clock/clock.js'],
+    output: {
+        filename: "app.bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new ExtractTextPlugin('style.css'),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/app.html'
+        }),
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+    ],
+    module: {
+		rules: [
+			{
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    use: ["css-loader", "sass-loader"],
+                    fallback: 'style-loader'
+                })
+			}
+		]
+	}
+}
