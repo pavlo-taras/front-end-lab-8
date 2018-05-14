9f8a09a2381ed80be3fdd7d63f30057c898817ee
Pavlo Taras <pavlo.taras1985@gmail.com>
2018-05-14 13:44:49 +0300

    Added code for homework 13

--- homework_13/src/app.js
+++ homework_13/src/app.js
@@ -1,3 +1,38 @@
-let dataPosts = getDataPosts();
+var app = angular.module('postPageApp', []);
 
-console.log("posts = ", dataPosts)
+app.controller('postController', function($scope) {
+    
+    $scope.title = "My Blog";
+    $scope.posts = getDefaultPosts()
+    $scope.tab = 'post';
+    $scope.currentPost = 0;
+
+    $scope.selectTab = function(setTab){
+        $scope.tab = setTab;
+    };
+    
+    $scope.isSelected = function(checkTab){            
+        return $scope.tab === checkTab;
+    };
+
+    $scope.enableEditor = function(iPost) {
+        $scope.currentPost = iPost;
+        $scope.selectTab('edit/' + $scope.currentPost.toString())
+        $scope.post = $scope.posts[$scope.currentPost];
+        $scope.post.description = [$scope.post.description];
+    };
+        
+    $scope.editPost = function() {
+        $scope.post.description = this.post["description"][0];
+        $scope.posts[$scope.currentPost] = $scope.post;
+        $scope.selectTab('post')
+    };
+
+    $scope.addPost = function(){
+        $scope.post.description = this.post["description"][0];
+        $scope.posts.unshift(this.post);
+        $scope.selectTab('post')
+    };  
+
+
+});
